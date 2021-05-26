dcaf3e8f2f74946b892aaf2d9554769ef14136c1
Paul <paul@lintcloud.com>
2021-05-26 17:27:41 +0530

    Uploaded If Condition of Python Program

---
+++ basic_mark.py
@@ -0,0 +1,7 @@
+mark = 70;
+if mark>80:
+    print("Excellent")
+elif mark>60:
+    print("Very Good")
+else:
+    print("Good")
---
+++ three_mark_conditions.py
@@ -0,0 +1,23 @@
+mark1 = int(input("Enter mark1 : "));
+mark2 = int(input("Enter mark2 : "));
+mark3 = int(input("Enter mark3 : "));
+
+if (mark1 == mark2) and (mark1 == mark3):
+    print("All marks are equal");
+else:
+    print("Marks are not equal");
+
+
+print("");
+print("===================================");
+print("");
+
+
+mark4 = int(input("Enter mark4 : "));
+mark5 = int(input("Enter mark5 : "));
+mark6 = int(input("Enter mark6 : "));
+
+if (mark4 == mark5) or (mark4 == mark6):
+    print("Marks are equal");
+else:
+    print("Marks are not equal");
